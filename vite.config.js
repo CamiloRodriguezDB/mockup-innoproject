6c2a33da32b88c1d49dd382651dcd6b62a53c222
Camilo Rodriguez <camilo.rodriguez-munoz@db-eco.com>
2025-06-10 16:08:56 -0500

    Vite Change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: '/mockup-innoproject',
+    base: '',
     build: {
       outDir: 'dist/react-showcase'
     },
